9240c2db3fdae43dc7de73a6cd53b9877eab0b5d
Alexandre Tellier <alex.tellier01@gmail.com>
2025-10-28 19:57:44 -0400

    added HTMLNode functionnality and tests

---
+++ src/htmlnode.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+from typing import List, Dict, Optional
+
+
+class HTMLNode:
+    def __init__(
+        self,
+        tag: Optional[str] = None,
+        value: Optional[str] = None,
+        children: Optional[List[HTMLNode]] = None,
+        props: Optional[Dict[str, str]] = None,
+    ) -> None:
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self) -> None:
+        raise NotImplementedError("children classes need to implement this method")
+
+    def props_to_html(self) -> str:
+        if self.props is None:
+            return ""
+
+        string_return = ""
+        for key in self.props:
+            string_return = f" {string_return} {key}={self.props[key]}"
+        return string_return
+
+    def __repr__(self) -> str:
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props_to_html()})"
---
+++ src/test_htmlnode.py
@@ -0,0 +1,25 @@
+import unittest
+from htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_to_html_raises_exception(self):
+        node = HTMLNode()
+        self.assertRaises(NotImplementedError, node.to_html)
+
+    def test_props_to_html_returns_empty_string_when_no_attributes(self):
+        node = HTMLNode()
+        prop_string = node.props_to_html()
+        self.assertEqual(prop_string, "")
+
+    def test_props_to_html(self):
+        tag = "html_tag"
+        value = "html_value"
+        children = "html_children"
+        props = {"prop1": "prop1_value", "prop2": "prop2_value"}
+
+        node = HTMLNode(tag, value, children, props)
+        test_text_string = (
+            f"HTMLNode({tag}, {value}, {children}, {node.props_to_html()})"
+        )
+        self.assertEqual(repr(node), test_text_string)
